d27e05c950b00d6627b1d5409be446f6824f51fb
Abishek.sk <skabishekofficialid@gmail.com>
2023-12-30 19:12:57 +0530

    Removed unwanted code

--- JsonManipulator/ThejsonCruderApp/admin.py
+++ JsonManipulator/ThejsonCruderApp/admin.py
@@ -30,31 +30,7 @@ admin.site.register(Relation, RelationAdmin)
 
 
 
-# admin.py
 
-# from django.contrib import admin
-# from .models import JsonData
-
-# class JsonDataAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'created_at', 'display_table_data')  # Add other fields as needed
-#     readonly_fields = ('display_table_data',)
-
-#     def display_table_data(self, obj):
-#         # Assuming the JSON data is stored as a list of dictionaries
-#         # You can customize this method based on your JSON structure
-#         table_html = '<table border="1">'
-#         if 'data' in obj:
-#             for item in obj['data']:
-#                 table_html += '<tr>'
-#                 for key, value in item.items():
-#                     table_html += f'<td>{key}</td><td>{value}</td>'
-#                 table_html += '</tr>'
-#         table_html += '</table>'
-#         return table_html
-
-#     display_table_data.short_description = 'Table Data'
-
-# admin.site.register(JsonData, JsonDataAdmin)
 
 
 
